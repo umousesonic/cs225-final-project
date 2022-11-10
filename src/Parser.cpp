f0b24ccd34559ae9e5ff167c9531b8bf5b7fe912
Tianshu Wei <tianshu@life-foundry.com>
2022-11-09 21:50:02 -0600

    Added documentations

--- src/Parser.cpp
+++ src/Parser.cpp
@@ -11,9 +11,16 @@ Parser::Parser() {
     filename_ = "";
 }
 
+/* readFile(string filename)
+ * Reads a .dat file and constructs csvector_
+ * parameters: filename, the path to the dat file
+ * returns: none
+ * constructs csvector_
+ */
 void Parser::readFile(std::string filename) {
     // extension name checking, skip for now
-    //
+
+    // Open file
     string line;
     ifstream datfile(filename);
 
@@ -31,9 +38,16 @@ void Parser::readFile(std::string filename) {
         }
         csvector_.push_back(v);
     }
+
+    // Close file
     datfile.close();
 }
-
+/* printCsvector()
+ * print out csvector_ for debugging.
+ * parameters: none
+ * returns: none
+ * prints the entire csvector for debugging.
+ */
 void Parser::printCsvector() {
     // Print code for debugging
     for (auto v : csvector_) {
